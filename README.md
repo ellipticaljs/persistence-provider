7a2fb2b71b7324238c65d0aae2fab13a1b648c4b
Scott <sfrancis@misinteractive.com>
2016-07-31 02:19:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+ELLIPTICAL PERSISTENCE PROVIDER
+===========================
+
+
+
+## Installation
+
+``` bash
+
+bower install elliptical-persistence-provider --save
+
+```
+
+## Usage
+
+```html
+<link rel="import" href="bower_components/elliptical-persistence-provider/elliptical-persistence-provider.html">
+
+
+```
---
+++ dist/elliptica.persistence.provider.js
@@ -0,0 +1,119 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'elliptical-generic-repository'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('elliptical-generic-repository'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.elliptical.GenericRepository);
+    global.provider = mod.exports;
+  }
+})(this, function (exports, _ellipticalGenericRepository) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _ellipticalGenericRepository2 = _interopRequireDefault(_ellipticalGenericRepository);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  var PersistenceProvider = function () {
+    function PersistenceProvider(key, $provider) {
+      _classCallCheck(this, PersistenceProvider);
+
+      this.key = key;
+      this.$provider = $provider;
+      this._model = this._getPersistenceModel();
+      this._repo = new _ellipticalGenericRepository2.default(this._model);
+      this._onChange(key, $provider);
+    }
+
+    _createClass(PersistenceProvider, [{
+      key: 'get',
+      value: function get(params, resource, query, callback) {
+        this._repo.get(params, resource, query, callback);
+      }
+    }, {
+      key: 'post',
+      value: function post(params, resource, callback) {
+        this._repo.post(params, resource, callback);
+      }
+    }, {
+      key: 'put',
+      value: function put(params, resource, callback) {
+        this._repo.put(params, resource, callback);
+      }
+    }, {
+      key: 'delete',
+      value: function _delete(params, resource, callback) {
+        this._repo.delete(params, resource, callback);
+      }
+    }, {
+      key: 'query',
+      value: function query(filter, asEnumerable) {
+        return this._model;
+      }
+    }, {
+      key: 'enumerable',
+      value: function enumerable() {
+        return this._repo.Enumerable();
+      }
+    }, {
+      key: 'clear',
+      value: function clear(callback) {
+        this._model = [];
+        this._repo.model = [];
+        if (callback) callback(null, []);
+      }
+    }, {
+      key: '_getPersistenceModel',
+      value: function _getPersistenceModel() {
+        var model = this.$provider.get(this.key);
+        if (model) return model;else return [];
+      }
+    }, {
+      key: '_onChange',
+      value: function _onChange(key, $provider) {
+        this._repo.onChange = function (model) {
+          $provider.set(key, model);
+        };
+      }
+    }]);
+
+    return PersistenceProvider;
+  }();
+
+  exports.default = PersistenceProvider;
+});
